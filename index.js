548cbc6f0fa4ce404d0596a64c0a5cdd6d09fd20
Omar A <oazzat19@gmail.com>
2018-09-11 19:15:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much."
+
+function moveDodgerRight(){
+  var rightNumbers = dodger.style.left.replace("px","")
+  var right = parseInt(rightNumbers,10)
+  
+  if (right<360){
+    dodger.style.right = `${left - 1}px`
+  }
+}
